9890a6734681bd9c9cb5ae5aad40e6e695cd4463
PaulAdr173 <pauadrian102@gmail.com>
2024-10-13 13:22:48 +0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,12 @@
+sir = "Nume: %s, Nota: %d"%("Paul",8)
+print(sir)
+
+sir = "iLikePython"
+x= sir[::-1]
+print(x)
+
+
+sir = ["iLikePython", "2"]
+print(len(sir))
+print('2' in sir)
+print('P' in sir[0])
---
+++ excurs.py
@@ -0,0 +1,20 @@
+txt = "Hello, welcome to my world."
+
+x = txt.rfind("e")
+y = txt.find("e")
+
+print(x)
+print(y)
+
+test = "iiiLikePythonLikeILikeI"
+print(test.index('Like'))
+print(test.rindex('Like'))
+
+sir = ("AB__CD__EF__GH")
+X = sir.split("__")[-1] 
+y =sir.split("__",1)[0]
+z = sir.split("__",2)[1]
+print(X)
+print(y)
+print(z)
+
